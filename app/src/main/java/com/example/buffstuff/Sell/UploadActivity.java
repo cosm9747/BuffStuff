d25d4b4c833a59d08a8a5a9bc334b26d04ac912c
Conor Smith <cosm9747@colorado.edu>
2019-11-17 01:37:03 -0700

    sell page functional

--- app/src/main/java/com/example/buffstuff/Sell/UploadActivity.java
+++ app/src/main/java/com/example/buffstuff/Sell/UploadActivity.java
@@ -1,5 +1,6 @@
 package com.example.buffstuff.Sell;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.buffstuff.Buy.BuyActivity;
 import com.example.buffstuff.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UploadActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -27,7 +42,43 @@ public class UploadActivity extends AppCompatActivity {
     }
 
     public void goToSell(View view) {
-        Intent intent = new Intent(this, SellActivity.class);
-        startActivity(intent);
+
+        EditText n = (EditText) findViewById(R.id.inputName);
+        EditText p = (EditText) findViewById(R.id.price);
+        Spinner con = (Spinner) findViewById(R.id.condition_spinner);
+        Spinner cat = (Spinner) findViewById(R.id.category_spinner);
+        String inputName = n.getText().toString();
+        Double price = Double.parseDouble(p.getText().toString());
+        String condition = con.getSelectedItem().toString();
+        String category = cat.getSelectedItem().toString();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String userId = currentUser.getUid();
+
+        Map<String, Object> item = new HashMap<>();
+
+        item.put("name", inputName);
+        item.put("price", price);
+        item.put("condition", condition);
+        item.put("category", category);
+        item.put("user", userId);
+
+        final Intent intent = new Intent(this, SellActivity.class);
+
+        db.collection("items")
+                .add(item)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        startActivity(intent);
+                        Log.d("dbSuccess", "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("dbFailure", "Error adding document", e);
+                    }
+                });
     }
 }
