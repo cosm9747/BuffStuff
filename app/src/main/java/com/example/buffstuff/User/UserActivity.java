3edd96a7ca7db2c6bac8d5cc3628f2d985d6150b
Conor Smith <cosm9747@colorado.edu>
2019-12-02 14:27:02 -0700

    password change for user

--- app/src/main/java/com/example/buffstuff/User/UserActivity.java
+++ app/src/main/java/com/example/buffstuff/User/UserActivity.java
@@ -24,6 +24,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -51,9 +53,9 @@ public class UserActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         String id = currentUser.getUid();
 
-        final TextView e = findViewById(R.id.textView9); //email
-        final EditText n = findViewById(R.id.editText5); //name
-        final EditText b = findViewById(R.id.editText6); //bio
+        final TextView e = (TextView) findViewById(R.id.textView9); //email
+        final EditText n = (EditText) findViewById(R.id.editText5); //name
+        final EditText b = (EditText) findViewById(R.id.editText6); //bio
 
         String email = currentUser.getEmail();
         e.setText(email);
@@ -86,11 +88,17 @@ public class UserActivity extends AppCompatActivity {
 
     public void updateInfo(View view) {
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        final FirebaseUser currentUser = mAuth.getCurrentUser();
         String id = currentUser.getUid();
 
-        final EditText n = findViewById(R.id.editText5); //name
-        final EditText b = findViewById(R.id.editText6); //bio
+        final TextView e = (TextView) findViewById(R.id.textView9); //email
+        final EditText n = (EditText) findViewById(R.id.editText5); //name
+        final EditText b = (EditText) findViewById(R.id.editText6); //bio
+        final EditText cp = findViewById(R.id.editpassword1);
+        final EditText np = findViewById(R.id.editpassword2);
+
+        final String currentPassword = cp.getText().toString();
+        final String newPassword = np.getText().toString();
 
         Map<String, Object> userInfo = new HashMap<>();
         userInfo.put("name", n.getText().toString());
@@ -100,8 +108,10 @@ public class UserActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Toast.makeText(UserActivity.this, "Successfully updated profile.",
-                                Toast.LENGTH_SHORT).show();
+                        if(newPassword.length() == 0 || currentPassword.length() == 0) {
+                            Toast.makeText(UserActivity.this, "Successfully updated profile.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                         Log.d("tag", "DocumentSnapshot successfully written!");
                     }
                 })
@@ -111,6 +121,45 @@ public class UserActivity extends AppCompatActivity {
                         Log.w("tag", "Error writing document", e);
                     }
                 });
+        //update password method
+        String email = currentUser.getEmail();
+        if(newPassword.length() != 0 && currentPassword.length() != 0) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            // Get auth credentials from the user for re-authentication. The example below shows
+            // email and password credentials but there are multiple possible providers,
+            // such as GoogleAuthProvider or FacebookAuthProvider.
+            AuthCredential credential = EmailAuthProvider
+                    .getCredential(email, currentPassword);
+
+            // Prompt the user to re-provide their sign-in credentials
+            currentUser.reauthenticate(credential)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                currentUser.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(UserActivity.this, "Successfully updated profile.",
+                                                    Toast.LENGTH_SHORT).show();
+                                            Log.d("PASS", "Password updated");
+                                        } else {
+                                            Log.d("PASS", "Error password not updated");
+                                        }
+                                    }
+                                });
+                            } else {
+                                Log.d("PASS", "Error auth failed");
+                            }
+                        }
+                    });
+        }
+        else if(newPassword.length() != 0 || currentPassword.length() != 0) {
+            Toast.makeText(UserActivity.this, "Can't change password when one field is empty",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     //Create an options menu
